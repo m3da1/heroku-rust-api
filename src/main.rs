007ca71f27036e86897932a1f836276f7ba4a88f
Mark Doh <mark.elorm.doh@gmail.com>
2021-10-03 22:13:14 +0000

    Change env and host addr

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use heroku_rust_api::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = std::env::var("ACTIX_PORT").unwrap_or("8000".into());
-    let address = format!("127.0.0.1:{}", port);
+    let port = std::env::var("PORT").unwrap_or("8000".to_string());
+    let address = format!("0.0.0.0:{}", port);
     println!("Starting web api @ {}", &address);
     let listener = TcpListener::bind(&address)?;
     run(listener)?.await
